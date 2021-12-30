78ae72379cc6257f328532f9773b197fa0d6abd2
Vitor R Munhoz <vmunhoz@ciandt.com>
2021-12-30 17:29:04 -0300

    Adding authenticated emails support

--- oauth2-proxy/rootfs/usr/bin/run.sh
+++ oauth2-proxy/rootfs/usr/bin/run.sh
@@ -6,6 +6,7 @@ main() {
     bashio::log.trace "${FUNCNAME[0]}"
 
     local email_domain=$(bashio::config 'email_domain')
+    local authenticated_emails=$(bashio::config 'authenticated_emails')
     local redirect_url=$(bashio::config 'redirect_url')
     local cookie_secure=$(bashio::config 'cookie_secure')
     local cookie_secret=$(bashio::config 'cookie_secret')
@@ -16,19 +17,35 @@ main() {
 
     bashio::log.info "Starting OAuth2 Proxy"
 
-    /usr/bin/oauth2-proxy \
-    --email-domain=${email_domain} \
-    --redirect-url=${redirect_url} \
-    --upstream=http://127.0.0.1:8080/ \
-    --cookie-secret=${cookie_secret}\
-    --cookie-secure=${cookie_secure} \
-    --cookie-refresh=${cookie_refresh} \
-    --provider=${provider} \
-    --reverse-proxy=true \
-    --set-authorization-header=true \
-    --client-id=${client_id} \
-    --client-secret=${client_secret} \
-    --http-address=0.0.0.0:4180
+    local oauth2_params=(
+        --upstream=http://127.0.0.1:8080/ \
+        --cookie-secret=${cookie_secret}\
+        --cookie-secure=${cookie_secure} \
+        --cookie-refresh=${cookie_refresh} \
+        --provider=${provider} \
+        --reverse-proxy=true \
+        --set-authorization-header=true \
+        --client-id=${client_id} \
+        --client-secret=${client_secret} \
+        --http-address=0.0.0.0:4180
+    )
+
+    # If redirect_url is NOT empty, add redirect-url param
+    [[ ! -z "$redirect_url" ]] && oauth2_params+=(--redirect-url=${redirect_url})
+
+    # If email_domain is NOT empty, add email-domain param
+    if [[ ! -z "$email_domain" ]] 
+    then
+        bashio::log.info "Configuring email-domain with: ${email_domain}"
+        oauth2_params+=(--email-domain=${email_domain})
+    else
+        bashio::log.info "Configuring authenticated-emails-file with: ${authenticated_emails}"
+        readarray -t arr2 < <(printf '%s\n' "${authenticated_emails}")
+        printf '%s\n' "${arr2[@]}" > /data/authenticated_emails.txt
+        oauth2_params+=(--authenticated-emails-file=/data/authenticated_emails.txt)
+    fi
+
+    /usr/bin/oauth2-proxy "${oauth2_params[@]}"
 }
 
 main "$@"
